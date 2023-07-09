9eab621526e0ee16ebe4221019ca6324ce5cd6fe
dimafili2412 <dimafili2412@gmail.com>
2023-07-09 22:41:49 +0300

    added products file (db imitation) added basic routes to get products and cart and manipulate cart added checkout route (placeholder - always successful) cart is managed using local var

--- index.js
+++ index.js
@@ -1,16 +1,68 @@
 const express = require('express');
 const path = require('path');
+const products = require('./products');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+//parse json
+app.use(express.json());
+
+// Cart state
+let cart = [];
+
+// Routes
+
+// Root
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Get cart
+app.get('/cart', (req, res) => {
+    res.send(cart);
+});
+
+// Get products
+app.get('/products', (req, res) => {
+    res.send(products);
+});
+
+// Add product to cart
+app.post('/cart/add', (req, res) => {
+    const { productIndex, planIndex } = req.body;
+    const product = products[productIndex];
+    const plan = product.plans[planIndex];
+    const item = {
+        title: product.name,
+        subtitle: plan.name,
+        price: plan.price,
+        img: product.img,
+    };
+    cart.push(item);
+    res.sendStatus(200);
+});
+
+// Remove product from cart
+app.post('/cart/remove', (req, res) => {
+    const { index } = req.body;
+    if (index >= 0 && index < cart.length) {
+        cart.splice(index, 1);
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+// Checkout
+app.post('/checkout', (req, res) => {
+    //placeholder, will always succeed
+    res.sendStatus(200);
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        name: 'Sample Product',
+        img: 'sample.jpg',
+        plans: [
+            {
+                name: 'Monthly Subscription',
+                price: 100,
+            },
+            {
+                name: 'Yearly Subscription',
+                price: 1200,
+            },
+        ],
+    },
+];
+
+module.exports = products;
